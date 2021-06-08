35b5bddb4ac38c1df0f63adf1aaea81439aa947a
duyle1430 <85335532+duyle1430@users.noreply.github.com>
2021-06-08 22:05:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Zịt pếu à!',
+    introTitle: 'Zợ iu à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim Đạt lẻ loi
+    Mà sao Nhi cứ đi nhầm đường
+    Lạc vào tim Zi lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng zịt đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng zịt đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích Đạt <33',
-    btnNo: 'Không, Anh sừng à :3',
+    title: 'Phải chăng zợ đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng zợ đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Iu Zi ngàn lần iu Zi <33',
+    btnNo: 'Zợ cũng iu Zi  :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Chiều mai 2h qua nhà Zi nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
